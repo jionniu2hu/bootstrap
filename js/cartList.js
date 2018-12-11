cf9c39c87693e090843db96dc98633ee63fbdb65
fortissimo <2655843602@qq.com>
2018-12-12 01:43:57 +0800

    5

--- js/cartList.js
+++ js/cartList.js
@@ -20,7 +20,7 @@ const operatorNameJso = {
 
 const operatorGlodal = {
     "clearAll": "[data-operator='clearAll']",
-    "deleteSelected": "[data-operator='deleteSelected']"
+    // "deleteSelected": "[data-operator='deleteSelected']"
 }
 // console.log(cart);
 // console.log(cart.getDataFromLocalStorage());
@@ -76,6 +76,14 @@ function displayOrderList() {
 
         // 移除当前订单节点到隐藏属性
         node.classList.remove('d-none');
+        // console.log(node);
+
+
+        // 克隆一个样本节点
+        // 设置一个新id
+        // 挂接到父元素   
+        // 获取所有到数据节点data - name 依次将对应到数据送入节点对应属性
+        // 移除新节点到隐藏属性 d - none
     }
     // 遍历订单列表(2)
     // for (let i = 0; i < orderList.length; i++) {
@@ -88,42 +96,34 @@ function displayOrderList() {
 //显示已选中商品道总件数和总价格
 function displaySelectedTotal() {
 
-    // 获取样本数节点并修改数据
     let totalNode = cartRoot.querySelector(dataNameJson.units);
     totalNode.textContent = cart.getTotalUnits();
-    // 获取选中商品数量节点并修改数据
+
     totalNode = cartRoot.querySelector(dataNameJson.selectedQty);
     totalNode.textContent = cart.getSelectedQty();
-    // 获取选中商品的总价格节点并修改数据
+
     totalNode = cartRoot.querySelector(dataNameJson.selectedAmount);
     totalNode.textContent = (cart.getSelectedAmount()).toFixed(2);
-    // 给删除按钮涉及一个data-id属性
+    // 删除选中商品
     element = node.querySelector(operatorNameJso.deleteItem);
-    element.setAttribute("data-id", Order.id);
-    // console.log(element);
+    element.setAttribute("data-id", order.id);
+    console.log(element);
 }
 
 // 为相关节点注册事件
 function regEvent() {
     // 获取清空购物车节点
     let element = cartRoot.querySelector(operatorGlodal.clearAll);
+    console.log(element);
     // 注册点击事件触发函数
     element.onclick = clearAllEventFun;
+
 
     // 获取删除购物车节点
-    let element = cartRoot.querySelectorAll(operatorNameJso.deleteItem);
-    // 为每个删除节点注册单机事件,事件触发函数
-    for (const i in element) {
-        element[i].onclick = deleteItemEventFun;
-    }
-    element.onclick = clearAllEventFun;
-
-
-    // 获取-的节点
-    // let element = cartRoot.querySelectorAll(operatorNameJso.deleteSelected);
-    // console.log(deleteid);
+    let deleteid = cartRoot.querySelector(operatorNameJso.deleteItem);
+    console.log(deleteid);
     // 注册点击事件触发函数
-    // element.onclick = deleteItemEventFun;
+    element.onclick = clearAllEventFun;
     // deleteid.onclick = deleteSelectedEventFun;
 }
 
@@ -141,20 +141,16 @@ function clearAllEventFun() {
     cartListNode.appendChild(ExampleNode);
     // 更新商品总数据
     displaySelectedTotal();
+    console.log('清空');
 }
 
 // 删除当前商品的触发函数
-function deleteItemEventFun() {
-
-    // 获取订单根节点
-    let cartListNode = document.querySelector('#cartList');
-    // 获取当前订单节点
-    let node = this.parentNode.parentNode;
-    // 调用购物车类删除订单函数
-    cart.deleteItem(node.id);
-    // 删除节点
-    cartListNode.removeChild(node);
-    // 修改各种总数据
+function deleteItemEventFun(e) {
+    let id = e.getattr.getAttribute("data-id");
+    // 删除购物车数据
+    cart.deleteItem(id);
+    let orderNode = cartList.getSelectedAmount(id);
+    cartList.removeChild(orderNode);
     displaySelectedTotal();
 }
 
@@ -169,4 +165,4 @@ function init() {
 
 }
 // 调用初始化函数
-init();
+init()
